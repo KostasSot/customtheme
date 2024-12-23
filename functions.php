2c229d9ffe8a14e97c9721b109ec9e5a22a9d8d0
KostasSot <ksotir98@gmail.com>
2024-12-24 00:28:33 +0200

    first commit

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php
+function wpdevs_load_scripts() {
+    wp_enqueue_style('wpdevs-style', get_stylesheet_uri(), array(), '1.0', ΄'all');
+}
+add_action('wp_enqueue_scripts', 'wpdevs_load_scripts');

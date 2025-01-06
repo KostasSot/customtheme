7694c6ad37459b3b132fc35b4bebe3c32d232b9e
KostasSot <ksotir98@gmail.com>
2025-01-07 00:51:00 +0200

    progress

--- footer.php
+++ footer.php
@@ -1,7 +1,14 @@
 <footer class="site-footer">
-            Footer
+            <div class="container">
+                <div class="copyright">
+                    <p>Copyright - All rights Reserved</p>
+                </div>
+                <div class="footer-menu">
+                    <?php wp_nav_menu(array('theme-location' => 'wp_devs_footer_menu', 'depth' => 1)) ?>
+                </div>
+            </div>
         </footer>
     </div>
-    
+    <?php wp_footer() ?>
 </body>
 </html>
